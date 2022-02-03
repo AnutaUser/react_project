b5719240b79ff55b1c6348654543033b3f77e9c3
AnutaUser <bezkorovainaanna048@gmail.com>
2022-02-03 20:48:57 +0200

    6

---
+++ enum.ts
@@ -0,0 +1,19 @@
+// export enum Enum {
+//     SUN = 5,
+//     MON,
+//     TEU,
+//     WED,
+//     THE,
+//     FRI,
+//     SET,
+// }
+
+export enum Enum {
+    SUN = 'SUN',
+    MON = 'MON',
+    TEU = 'TEU',
+    WED = 'WED',
+    THE = 'THE',
+    FRI = 'FRI',
+    SET = 'SET',
+}
