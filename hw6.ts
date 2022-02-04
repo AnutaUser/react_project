893ea427d34e8811e308e465471ab4f365de7962
AnutaUser <bezkorovainaanna048@gmail.com>
2022-02-04 16:46:46 +0200

    hw6

---
+++ hw6.ts
@@ -0,0 +1,115 @@
+// 1) создать интерфейс на основе этого объекта:
+//
+import {IMission} from "./interface/IMission.interface";
+
+const mission:IMission = {
+    "mission_name": "Starlink-15 (v1.0)",
+    "launch_date_local": "2020-10-24T11:31:00-04:00",
+    "launch_site": {
+    "site_name_long": "Cape Canaveral Air Force Station Space Launch Complex 40"
+},
+    "links": {
+    "article_link": null,
+        "video_link": "https://youtu.be/J442-ti-Dhg"
+},
+    "rocket": {
+    "rocket_name": "Falcon 9",
+        "first_stage": {
+        "cores": [
+            {
+                "flight": 7,
+                "core": {
+                    "reuse_count": 6,
+                    "status": "unknown"
+                }
+            }
+        ]
+    },
+    "second_stage": {
+        "payloads": [
+            {
+                "payload_type": "Satellite",
+                "payload_mass_kg": 15400,
+                "payload_mass_lbs": 33951.2
+            }
+        ]
+    }
+}
+}
+// -----------------------------------------------------------------------------------------------
+
+// import {IMission} from "./interface/mission.interface";
+//
+// const mission:Partial<IMission> = {
+//     mission_name: "mission_name",
+//     launch_date_local: "launch_date_local",
+//     launch_site: {site_name_long:'site_name_long'}
+// }
+
+console.log(mission)
+
+// ---------------------------------------------------------------------------—————————
+// 2) протипизировать функции:
+//
+// import {IUser} from "./interface/IUser.interface";
+// import {IShapes} from "./interface/IShapesInterface";
+//
+// const user: IUser = {
+//     name: "Max",
+//     age: 18,
+//     gender: 'male'
+// };
+//
+// // function sum(a,b){
+// //     return a+b
+// // }
+//
+// class sum implements IShapes {
+//     add(): number {
+//         return this.a + this.b;
+//     }
+//     constructor(private a: number, private b: number) {
+//     }
+// }
+//
+// class showSum implements IShapes {
+//     add(): number {
+//         return this.a + this.b;
+//     }
+//     constructor(private a: number, private b: number) {
+//     }
+// }
+// // function showSum(a,b){
+// //     console.log(a + b);
+// // }
+//
+// class incAge implements IShapes {
+//     add(): number {
+//         return user.age += this.inc;
+//     }
+//
+//     constructor(private user: IUser, private inc: number) {
+//
+//     }
+// }
+//
+// // function incAge(someUser, inc){
+// //     someUser.age+=inc
+// //     return someUser
+// // }
+//
+// // console.log(new sum(1, 2));
+// // console.log(new showSum(2,3));
+// // console.log(new incAge(user, 2));
+//
+// const shapes: IShapes [] = [
+//     new sum(1,2),
+//     new showSum(2,3),
+//     new incAge(user,2)
+// ];
+//
+// console.log(shapes);
+//
+// for (const shape of shapes) {
+//     console.log(shape.constructor["name"], shape.add());
+// }
---
+++ interface/IMission.interface.ts
@@ -0,0 +1,82 @@
+// export interface IMission {
+//     mission_name: string;
+//     launch_date_local: string;
+//     launch_site: {
+//         site_name_long: string;
+//         };
+//     links: {
+//         article_link: null;
+//         video_link: string;
+//         };
+//     rocket: {
+//         rocket_name: string;
+//         first_stage: {
+//             cores: [
+//                 {
+//                     flight: number;
+//                     core: {
+//                         reuse_count: number;
+//                         status: string;
+//                         }
+//                 }
+//             ]
+//         };
+//         second_stage: {
+//             payloads: [
+//                 {
+//                     payload_type: string;
+//                     payload_mass_kg: number;
+//                     payload_mass_lbs: number;
+//                 }
+//             ]
+//         }
+//     }
+// }
+
+export interface IMission {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: ILaunch_site;
+    links: ILinks;
+    rocket: IRocket;
+}
+
+export interface ILaunch_site {
+    site_name_long: string;
+}
+
+export interface ILinks {
+    article_link: null;
+    video_link: string;
+}
+
+export interface IRocket {
+    rocket_name: string;
+    first_stage: IFirst_stage;
+    second_stage: ISecond_stage;
+}
+
+export interface IFirst_stage {
+    cores: ICores[];
+}
+
+export interface ICores {
+    flight: number,
+    core: ICore;
+}
+
+export interface ICore {
+    reuse_count: number;
+    status: string;
+}
+
+export interface ISecond_stage {
+    payloads: IPayloads[];
+}
+
+export interface IPayloads {
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number;
+
+}
